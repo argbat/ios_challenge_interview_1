c01d1a409903ea7a34d5b809bc3a34ff2de750ef
Demian Odasso <argbat@gmail.com>
2023-02-03 16:01:14 -0300

    project created

---
+++ CabifyShop/CabifyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabifyShopApp.swift
+//  CabifyShop
+//
+//  Created by Demian Odasso on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CabifyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CabifyShopTests/CabifyShopTests.swift
@@ -0,0 +1,11 @@
+//
+//  CabifyShopTests.swift
+//  CabifyShopTests
+//
+//  Created by Demian Odasso on 03/02/2023.
+//
+
+import XCTest
+
+final class CabifyShopTests: XCTestCase {
+}
